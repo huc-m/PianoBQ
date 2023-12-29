6ff6f81fcfc38b3a9be15fc3621ad12ca22c0c0d
huc-m <89653809+huc-m@users.noreply.github.com>
2023-12-29 12:44:41 +0200

    Create constants.h

---
+++ midi/constants.h
@@ -0,0 +1,18 @@
+#ifndef GLOBAL_CONSTANTS_H
+#define GLOBAL_CONSTANTS_H
+
+#define NULL_FILE_NAME "/dev/null"
+#define TUNE_LENGTH_MAX 5000
+
+#define LE_H 0  // left hand
+#define RI_H 1  // right hand
+//#define NOTES8    // if definet max notes = 8 else 4
+
+//error on read mmidi file MIDI_Fluidsynth::read_midi_file
+#define MIDI_FILE_TOO_MANY_NOTES_IN_TUPLE 3
+#define MIDI_FILE_TUNE_IS_TOO_LONG 2
+#define MIDI_FILE_ERROR_ON_READING 1
+
+//MIDI status byte
+#define MIDI_NOTE_ON 0x90
+#endif // GLOBAL_CONSTANTS_H
